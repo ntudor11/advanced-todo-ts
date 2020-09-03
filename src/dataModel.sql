a1b32c2bd303c2f1efb659a6d9c90aa2a5cdd8ad
Tudor Nastasa <tudorrr.nastasa@gmail.com>
2020-09-03 23:38:59 +0200

    user todos api

--- src/dataModel.sql
+++ src/dataModel.sql
@@ -14,9 +14,10 @@ create table todos (
   id integer not null primary key,
   task text,
   description text,
+  deadline text,
   priority text,
-  status_id text unique references status(id),
-  user_id integer unique references users(id)
+  status_id text references status(id),
+  user_id integer references users(id)
 );
 
 create table tags (
@@ -38,4 +39,102 @@ insert into users (
   "abc@abc.com",
   "password",
   "https://cnaca.ca/wp-content/uploads/2018/10/user-icon-image-placeholder.jpg"
+);
+
+insert into status (name) values ("Backlog");
+
+insert into status (name) values ("To Do");
+
+insert into status (name) values ("Doing");
+
+insert into status (name) values ("Done");
+
+insert into users (
+  email,
+  password,
+  image
+) values (
+  "ab@ab.com",
+  "password",
+  "https://cnaca.ca/wp-content/uploads/2018/10/user-icon-image-placeholder.jpg"
+);
+
+insert into todos (
+  task,
+  description,
+  priority,
+  status_id,
+  user_id
+) values (
+  "check email",
+  "check yahoo, itu, all  emails",
+  "Medium",
+  2,
+  1
+);
+
+insert into todos (
+  task,
+  description,
+  priority,
+  status_id,
+  user_id
+) values (
+  "eat green",
+  "<ul><li>chickpeas</li><li>broccoli</li></ul>",
+  "High",
+  3,
+  2
+);
+
+insert into todos (
+  task,
+  description,
+  priority,
+  status_id,
+  user_id
+) values (
+  "sport",
+  "<ul><li>pushups</li><li>squats</li></ul>",
+  "Low",
+  1,
+  1
+);
+
+insert into tags (name, color) values (
+  "work",
+  "#ff0000"
+);
+
+insert into tags (name, color) values (
+  "private",
+  "#00ff00"
+);
+
+insert into todos_tags (
+  todo_id,
+  tag_id
+) values (
+  1, 2
+);
+
+insert into todos_tags (
+  todo_id,
+  tag_id
+) values (
+  1, 1
+);
+
+insert into todos_tags (
+  todo_id,
+  tag_id
+) values (
+  2, 1
+);
+
+insert into todos_tags (
+  todo_id,
+  tag_id
+) values (
+  3, 2
 );
--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,7 @@ import express from "express";
 const bodyParser = require("body-parser");
 const Database = require("better-sqlite3");
 const cors = require("cors");
+const jwt = require("jsonwebtoken");
 
 const app = express();
 const db = new Database("src/todo.db");
@@ -12,17 +13,105 @@ app.use(cors());
 app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
+const secret = "TEMP_SECRET";
+
+const withAuth = (requiredLevel?: string) => (
+  req: any,
+  res: any,
+  next: any
+) => {
+  req.cookies = {
+    token: undefined,
+  };
+  const { token } = req.cookies;
+  if (!token) {
+    res.status(401).send({ error: "Unauthorized: No token provided" });
+  } else {
+    jwt.verify(token, secret, (err: any, decoded: any) => {
+      if (err) {
+        res.status(401).send({ error: "Unauthorized: Invalid token" });
+      } else if (requiredLevel && !requiredLevel.includes(decoded.type)) {
+        next();
+        res.status(401).send({ error: "Unauthorized: Invalid user type" });
+      } else {
+        req.userId = decoded.userId;
+        req.email = decoded.email;
+        req.type = decoded.type;
+        req.image = decoded.image;
+        next();
+      }
+    });
+  }
+};
+
 app.get("/users", (req, res) => {
   const users = db
     .prepare(
       `
-      select *
+      select id, email, image
       from users
     `
     )
     .all();
 
   res.send(users);
+});
+
+app.get("/user/:id", withAuth(undefined), (req, res) => {
+  const { id } = req.params;
+  const user = db
+    .prepare(
+      `
+      select *
+      from users
+      where id = ?
+    `
+    )
+    .get(id);
+
+  const todos = db
+    .prepare(
+      `
+        select t.id, t.task, t.description, t.priority, t.deadline from todos t
+          join users u
+          on u.id = t.user_id
+          where t.user_id = ?
+      `
+    )
+    .all(id);
+
+  todos.map((todo: any) => {
+    const todoTags = db
+      .prepare(
+        `
+            select distinct tg.id, tg.name, tg.color
+              from tags tg
+              join todos_tags tt
+              on tg.id = tt.tag_id
+              join todos t
+              on t.id = tt.todo_id
+              where t.id = ?;
+          `
+      )
+      .all(todo.id);
+
+    const todoStatus = db
+      .prepare(
+        `
+          select distinct s.id, s.name
+            from status s
+            join todos t
+            on s.id = t.status_id
+            where t.id = ?;
+        `
+      )
+      .get(todo.id);
+
+    todo.tags = todoTags;
+    todo.status = todoStatus;
+  });
+
+  res.send({ user, todos });
 });
 
 app.listen(PORT, () => {
